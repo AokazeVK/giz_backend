98bfc65b8108a4fb6dba79979eac9d9b7f99b5c3
CristhianSilvaMelgar <crissilva.cs77@gmail.com>
2025-09-25 21:16:53 -0400

    feat: urls asesoramientos

--- preparacion/urls.py
+++ preparacion/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import EmpresaViewSet, PublicacionEmpresaComunidadViewSet
+from .views import ArchivoAsesoramientoViewSet, AsesoramientoViewSet, EmpresaViewSet, EncargadoAsesoramientoViewSet, PublicacionEmpresaComunidadViewSet, SolicitudAsesoramientoViewSet
 
 router = DefaultRouter()
 router.register(r"empresas", EmpresaViewSet, basename="empresa")
 router.register(r"publicaciones-comunidad", PublicacionEmpresaComunidadViewSet, basename="publicacion-comunidad")
-
+router.register(r"solicitud-asesoramiento", SolicitudAsesoramientoViewSet, basename="solicitud-asesoramiento")
+router.register(r"asesoramiento", AsesoramientoViewSet, basename="asesoramiento")
+router.register(r"asesoramiento-archivos", ArchivoAsesoramientoViewSet, basename="asesoramiento-archivos")
+router.register(r"asesoramiento-encargados", EncargadoAsesoramientoViewSet, basename="asesoramiento-encargados")
 urlpatterns = router.urls
