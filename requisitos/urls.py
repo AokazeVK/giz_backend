0b41756af14d311c273aca04e3df1d8e22d86b94
AokazeVK <joel.huayhua@gmail.com>
2025-09-26 01:03:57 -0400

    add: capacitacoin url, enlaces url

--- requisitos/urls.py
+++ requisitos/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
+    EnlacesViewSet,
     EvaluacionDatoViewSet,
     RequisitoInputValorViewSet,
     TipoSelloViewSet, 
@@ -22,6 +23,7 @@ router.register(r'evaluaciones', EvaluacionViewSet, basename='evaluaciones')
 router.register(r'evaluacion-fases', EvaluacionFasesViewSet, basename='evaluacion-fases')  # Nueva ruta
 router.register(r'requisitos-valores', RequisitoInputValorViewSet, basename='requisitos-valores')  # Nueva ruta
 router.register(r'evaluacion-dato', EvaluacionDatoViewSet, basename='evaluacion-dato')  # Nueva ruta
+router.register(r'enlaces', EnlacesViewSet, basename='enlaces')  # Nueva ruta
 
 # Las URL generadas por el router se incluyen en tu URL principal
 urlpatterns = [
